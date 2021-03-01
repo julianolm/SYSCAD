ca0fbd754d1240254bbc1fc152e25adf03177d9f
Juliano Amadeu <julianolopesmoura4@gmail.com>
2021-03-01 15:29:19 -0300

    Adicionando cor de fundo a view principal

--- v3/view_principal.py
+++ v3/view_principal.py
@@ -5,14 +5,15 @@ import sys
 class Window(QWidget):
     def __init__(self):
         super().__init__()
-        self.setGeometry(200,200,800,600)
+        self.setGeometry(200,200,1000,700)
+        self.setStyleSheet("Background-color: rgb(7, 105, 114);")
 
 def executa():
 
     myApp = QApplication.instance()
 
     if myApp is None:
-        myApp = QApplication(sys.agv)
+        myApp = QApplication(sys.argv)
 
     janela = Window()
     janela.show()
