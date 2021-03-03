283cd3a12c1999bb908c7f1ce6575260414f62a8
Juliano Amadeu <julianolopesmoura4@gmail.com>
2021-03-03 15:33:19 -0300

    Desenvolvimento do frame 'Cadastrar'

--- v3/view_principal.py
+++ v3/view_principal.py
@@ -1,4 +1,4 @@
-from PySide2.QtWidgets import QWidget, QApplication, QPushButton, QFrame
+from PySide2.QtWidgets import QWidget, QApplication, QPushButton, QFrame, QLabel, QLineEdit
 from PySide2.QtGui import QFont
 
 import sys
@@ -48,8 +48,32 @@ class Window(QWidget):
         global frm_cadastrar
         self.frm_cadastrar = QFrame(self)
         self.frm_cadastrar.setGeometry(170, 0, 830, 700)
-        self.frm_cadastrar.setStyleSheet("background-color: rgb(255, 0, 0);")
+        self.frm_cadastrar.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.frm_cadastrar.setVisible(False)
+
+        self.lbl_nome = QLabel("Nome", self.frm_cadastrar)
+        self.lbl_nome.setGeometry(20, 50, 55, 16)
+        
+        self.lbl_endereco = QLabel("Endereco", self.frm_cadastrar)
+        self.lbl_endereco.setGeometry(20, 90, 55, 16)
+
+        self.lbl_cpf = QLabel("CPF", self.frm_cadastrar)
+        self.lbl_cpf.setGeometry(20, 130, 55, 16)
+
+        self.txt_nome = QLineEdit(self.frm_cadastrar)
+        self.txt_nome.setGeometry(80, 50, 721, 22)
+
+        self.txt_endereco = QLineEdit(self.frm_cadastrar)
+        self.txt_endereco.setGeometry(80, 90, 721, 22)
+
+        self.txt_cpf = QLineEdit(self.frm_cadastrar)
+        self.txt_cpf.setGeometry(80, 130, 721, 22)
+
+        self.btn_limpar = QPushButton('Limpar', self.frm_cadastrar)
+        self.btn_limpar.setGeometry(20, 650, 115, 22)
+
+        self.btn_gravar = QPushButton('Gravar', self.frm_cadastrar)
+        self.btn_gravar.setGeometry(700, 650, 115, 22)
 
 
         '''
@@ -125,3 +149,5 @@ def executa():
     janela = Window()
     janela.show()
     myApp.exec_()
+
+executa()
