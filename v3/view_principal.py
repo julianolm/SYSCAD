cae8808bdc843d1f300023d711125d9a7b443faf
Juliano Amadeu <julianolopesmoura4@gmail.com>
2021-03-03 16:09:47 -0300

    Concertando problema do label Endereco no frame Cadastrar
    O texto do label nao estava aparecendo todo. Seu tamanho foi aumentado e todas as caixas de texto passaram a comecar 10px a frente do que estavam (80 -> 90).

--- v3/view_principal.py
+++ v3/view_principal.py
@@ -54,19 +54,19 @@ class Window(QWidget):
         self.lbl_nome.setGeometry(20, 50, 55, 16)
         
         self.lbl_endereco = QLabel("Endereco", self.frm_cadastrar)
-        self.lbl_endereco.setGeometry(20, 90, 55, 16)
+        self.lbl_endereco.setGeometry(20, 90, 65, 16)
 
         self.lbl_cpf = QLabel("CPF", self.frm_cadastrar)
         self.lbl_cpf.setGeometry(20, 130, 55, 16)
 
         self.txt_nome = QLineEdit(self.frm_cadastrar)
-        self.txt_nome.setGeometry(80, 50, 721, 22)
+        self.txt_nome.setGeometry(90, 50, 721, 22)
 
         self.txt_endereco = QLineEdit(self.frm_cadastrar)
-        self.txt_endereco.setGeometry(80, 90, 721, 22)
+        self.txt_endereco.setGeometry(90, 90, 721, 22)
 
         self.txt_cpf = QLineEdit(self.frm_cadastrar)
-        self.txt_cpf.setGeometry(80, 130, 721, 22)
+        self.txt_cpf.setGeometry(90, 130, 721, 22)
 
         self.btn_limpar = QPushButton('Limpar', self.frm_cadastrar)
         self.btn_limpar.setGeometry(20, 650, 115, 22)
@@ -89,7 +89,7 @@ class Window(QWidget):
         self.lbl_nome.setGeometry(20, 50, 55, 16)
 
         self.txt_nome = QLineEdit(self.frm_pesquisar)
-        self.txt_nome.setGeometry(80, 50, 600, 22)
+        self.txt_nome.setGeometry(90, 50, 600, 22)
 
         self.btn_pesquisar = QPushButton('Pesquisar', self.frm_pesquisar)
         self.btn_pesquisar.setGeometry(700, 50, 80, 22)
